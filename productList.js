ca3355d0bd6cbbdb6bf1733bf67228dc2fe640e1
pankaj143s <pankajsarnobat143@gmail.com>
2024-08-17 23:49:19 +0530

    Completed addd to cart fun - Major UI change

---
+++ productList.js
@@ -0,0 +1,156 @@
+const productList = [
+    //group1
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-1.jpg",
+        title: "Yellow Book",
+        price: 25,
+        category: "Necessity"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-2.jpg",
+        title: "Black bag",
+        price: 30,
+        category: "Necessity"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-3.jpg",
+        title: "Nike Shoes",
+        price: 50,
+        category: "Fashion"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-4.jpg",
+        title: "Woman bag",
+        price: 50,
+        category: "Necessity"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-5.jpg",
+        title: "Fancy hat",
+        price: 35,
+        category: "Necessity"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-6.jpg",
+        title: "Coffee cup",
+        price: 20,
+        category: "Necessity"
+    },
+    //group2
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-7.jpg",
+        title: "Desk lamp",
+        price: 50,
+        category: "Design"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img8.jpg",
+        title: "White T-shirt",
+        price: 26,
+        category: "Design"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-9.jpg",
+        title: "Black bottle",
+        price: 30,
+        category: "Design"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-10.jpg",
+        title: "Warm blanket",
+        price: 40,
+        category: "Design"
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-11.jpg",
+        title: "Cool backpack",
+        price: 55,
+        category: "Design"
+    },
+    ,
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-12.jpg",
+        title: "Tea cup",
+        price: 35,
+        category: "Design"
+    },
+    //group3
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-13.jpg",
+        title: "Red apron",
+        price: 22,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-14.jpg",
+        title: "Picture frame",
+        price: 35,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-15.jpg",
+        title: "Home plant",
+        price: 40,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-16.jpg",
+        title: "Black umbrella",
+        price: 25,
+        category: ""
+    },
+    //group4
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-17.jpg",
+        title: "White blouse",
+        price: 55,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-18.jpg",
+        title: "Fancy blouse",
+        price: 60,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-19.jpg",
+        title: "Elegant T-shirt",
+        price: 45,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-20.jpg",
+        title: "Colorful dress",
+        price: 65,
+        category: ""
+    },
+    //group5
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-21.jpg",
+        title: "Abstract picture",
+        price: 70,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-22.jpg",
+        title: "Minimal frame",
+        price: 65,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-23.jpg",
+        title: "Minimal frame",
+        price: 72,
+        category: ""
+    },
+    {
+        img: "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/02/shop-img-24.jpg",
+        title: "Minimal design",
+        price: 78,
+        category: ""
+    },
+
+    // Add more products here
+];
+
+export default productList;
